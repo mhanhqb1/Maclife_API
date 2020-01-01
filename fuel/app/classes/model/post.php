928e335da2808b889d44391d7c6b5f1049c4e607
AnhMH <anh.mh@moziasoft.com>
2020-01-01 10:55:18 +0700

    Modify api post all

--- fuel/app/classes/model/post.php
+++ fuel/app/classes/model/post.php
@@ -321,6 +321,9 @@ class Model_Post extends Model_Abstract {
         if (!empty($param['name'])) {
             $query->where(self::$_table_name.'.name', 'LIKE', "%{$param['name']}%");
         }
+        if (!empty($param['created'])) {
+            $query->where(self::$_table_name.'.created', '>=', $param['created']);
+        }
         if (!empty($param['cate_id'])) {
             if (!is_array($param['cate_id'])) {
                 $param['cate_id'] = explode(',', $param['cate_id']);
